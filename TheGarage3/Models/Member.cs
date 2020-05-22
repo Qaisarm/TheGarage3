117baf30e227b6acb7f881a0507c08d011318eb1
Qaisarm <qmphy99@gmail.com>
2020-05-22 03:05:49 +0200

    Add project files.

---
+++ TheGarage3/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheGarage3.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public ICollection<ParkedVehicle> ParkedVehicles { get; set; }
+    }
+}
---
+++ TheGarage3/Models/ParkedVehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheGarage3.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        public string RegNo { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NoOfWheels { get; set; }
+
+        public int MemberId { get; set; }
+        public DateTime CheckInTime { get; set; }
+       
+
+        //Navigationproperty Not in database!
+        public Member Member { get; set; }
+    }
+}
